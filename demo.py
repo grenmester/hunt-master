2236a7dad96a862da3532c89523881cce6a98fdb
Owen-Gillespie <phin1797@gmail.com>
2017-04-23 00:40:03 -0700

    Added a test image for the demo

--- demo.py
+++ demo.py
@@ -1,7 +1,7 @@
 import seek
 DOMAIN = "seek.com"
 
-start = seek.StartModule("<p> HI!</p>", "test1")
+start = seek.StartModule("""<p> HI!</p><img src="static/img/beach_hacks_logo.jpg" alt="Giant Glass Pyramid">""", "test1")
 test1 = seek.TextModule('test1', "textinput", "BYE!")
 test2 = seek.TextInputModule("textinput", "showersuite", "Test!")
 test2 = seek.GPSModule("showersuite", "findtest", "1.775", "2.7775")
