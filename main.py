9739c5eb39d326b2d94091e60f8cfce4c7a5ddaa
Jacky Lee <dr.jackylee@gmail.com>
2017-04-22 22:51:54 -0700

    Add image uploading

--- main.py
+++ main.py
@@ -1,17 +1,23 @@
-from flask import Flask, render_template, redirect
-import json
+from flask import Flask, render_template, request, redirect, url_for, send_from_directory
+from werkzeug import secure_filename
+import json, os
 app = Flask(__name__)
 
-with open("modules.json", 'r') as fp:
+with open('modules.json', 'r') as fp:
     layout = json.load(fp)
+
+app.config['UPLOAD_FOLDER'] = 'uploads/'
+app.config['ALLOWED_EXTENSIONS'] = set(['png', 'jpg', 'jpeg', 'gif'])
+
+#### App Routes
 
 @app.route('/')
 def main():
-    return redirect("start/", code=302)
+    return redirect('start', code=302)
 
 @app.route('/start/')
 def start():
-    return render_template("start.html", start_link = layout["start"]["target"])
+    return render_template('start.html', start_link = layout['start']['target'])
 
 @app.route('/content/<module>/')
 def get_content_module(module):
@@ -22,11 +28,29 @@ def get_content_module(module):
 
 @app.route('/end/')
 def end():
-    return render_template("end.html")
+    return render_template('end.html')
 
 @app.errorhandler(404)
 def not_found_error(error):
     return render_template('404.html'), 404
+
+#### Other Functions
+
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1] in app.config['ALLOWED_EXTENSIONS']
+
+@app.route('/upload/', methods=['POST'])
+def upload():
+    file = request.files['file']
+    if file and allowed_file(file.filename):
+        filename = secure_filename(file.filename)
+        file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+        return redirect(url_for('uploaded_file', filename = filename))
+
+@app.route('/uploads/<filename>')
+def uploaded_file(filename):
+    return send_from_directory(app.config['UPLOAD_FOLDER'], filename)
 
 if __name__ == '__main__':
     app.run(debug=True)
