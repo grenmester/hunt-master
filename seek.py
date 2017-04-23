8b159dec931371c552e94124dffac614af481aa5
Owen-Gillespie <phin1797@gmail.com>
2017-04-22 18:12:34 -0700

    Change state.json to map between url's instead of just module names

--- seek.py
+++ seek.py
@@ -21,15 +21,31 @@ class Module:
 class ContentModule(Module):
 
 	def __init__(self,name, html_body, link_to):
-		super().__init__("/content/" + name, link_to)
+		super().__init__(name, link_to)
 		self.html_body = html_body
 		self.link_to = link_to
-		module_data[self.name] = (html_body,)
+		self.url = "/content/" + self.name
+		module_data[self.name] = (self.url, html_body)
+
+# class TextModule(ContentModule):
+# 	def __init__(self, name, link_to, text, ):
+# 		html_body
+
 
 def save_state_graph(filename = "state.json"):
+	url_state_graph = {}
+	for module_name in state_graph:
+
+		module_url = module_data[module_name][0]
+		target_module = state_graph[module_name]
+		target_url = module_data[target_module][0]
+
+		url_state_graph[module_url] = target_url		
+
 	with open(filename, 'w') as fp:
-		json.dump(state_graph, fp)
+		json.dump(url_state_graph, fp)
 
 def save_module_data(filename = "modules.json"):
 	with open(filename, 'w') as fp:
 		json.dump(module_data, fp)
+
