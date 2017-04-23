5b90f8bcf8c6b73cfbbd55e9ea5116f102e26763
Jacky Lee <dr.jackylee@gmail.com>
2017-04-22 18:21:29 -0700

    Redirect root to content/start

--- main.py
+++ main.py
@@ -1,9 +1,13 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect
 app = Flask(__name__)
 
 @app.route('/')
 def main():
-    return render_template('index.html')
+    return redirect("content/start/", code=302)
+
+@app.route('/content/start/')
+def start():
+    return render_template("index.html")
 
 if __name__ == '__main__':
     app.run(debug=True)
